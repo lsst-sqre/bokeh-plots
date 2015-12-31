479cb1a24d0add64c146c69e04b4a8a17ec0ccac
Angelo Fausti Neto <angelofausti@gmail.com>
2015-12-30 18:07:27 -0700

    Added scatter_plot_app example

---
+++ scatter_plot_app/app.py
@@ -0,0 +1,55 @@
+import pandas as pd
+import jinja2
+from bokeh.plotting import figure, show, output_file
+from bokeh.models import ColumnDataSource
+from bokeh.embed import components
+
+from flask import Flask
+app = Flask(__name__)
+
+def make_plot():
+	data = pd.read_csv("../test/sample.csv", usecols=['ra', 'dec'])
+	source = ColumnDataSource(data)
+	plot = figure(webgl=True)
+	plot.scatter('ra','dec', source=source, alpha=0.1)
+
+	return plot
+
+
+template = jinja2.Template("""
+<!DOCTYPE html>
+<html lang="en-US">
+
+<link
+    href="http://cdn.pydata.org/bokeh/release/bokeh-0.10.0.min.css"
+    rel="stylesheet" type="text/css"
+>
+<script 
+    src="http://cdn.pydata.org/bokeh/release/bokeh-0.10.0.min.js"
+></script>
+
+<body>
+
+    <h1>Scatter plot example</h1>
+
+    <p> bokeh plot embedded in a flask app, using jinga2 for HTML templating</p>
+    
+    {{ script }}
+    
+    {{ div }}
+
+</body>
+
+</html>
+""")
+
+p=make_plot()
+script, div = components(p)
+
+@app.route('/')
+def example():
+    return template.render(script=script, div=div)
+
+app.run(port=5050)
+
+
