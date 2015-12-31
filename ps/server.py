d4e8163f4d80d074f5e8cd4386df27913c3dc251
Angelo Fausti Neto <angelofausti@gmail.com>
2015-12-30 22:39:21 -0700

    Update server.py

--- ps/server.py
+++ ps/server.py
@@ -6,27 +6,25 @@ import pickle
 n_max=0
 stats={}
 
-butler = Butler('/home/afausti/bulge/output.1')
+# output dir of the stack processing
+
+output_dir = ""
+butler = Butler(output_dir)
 
 for dataref in butler.subset(datasetType='src'):
 
     if dataref.datasetExists(): # processrCcd did not fail
 
         visit = dataref.dataId['visit']
-        
-        src=dataref.get()
-
+        src = dataref.get()
         n = len(src)
-        
         print visit, n		
-
         if visit in stats:
             stats[visit].append(n)
         else:
             stats[visit]=list()
             stats[visit].append(n)
-
 print stats
 
-pickle.dump(stats, open( "ps.pkl", "wb"))
+pickle.dump(stats, open( "../test/ps.pkl", "wb"))
 
