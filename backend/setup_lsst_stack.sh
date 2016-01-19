20949f992a90a473083a22203cd591516a541d45
Angelo Fausti - afausti@lsst.org - LSST DM SQuaRE <afausti@lsst-dev.ncsa.illinois.edu>
2016-01-19 15:29:45 -0600

    Added script to setup lsst stack packages needed to run the examples

---
+++ backend/setup_lsst_stack.sh
@@ -0,0 +1,44 @@
+
+tag=w_2015_40
+
+export INSTALL_DIR=/home/afausti/lsst
+source $INSTALL_DIR/loadLSST.bash
+
+echo "Setting up stack $tag ..."
+
+setup -t $tag pipe_tasks
+setup -t $tag ctrl_orca
+setup -t $tag ctrl_execute
+setup -t $tag ctrl_platform_lsst
+setup -t $tag ctrl_stats
+setup -t $tag display_ds9
+
+# The following packages are not included in the stack distribution
+
+echo "Updating obs_decam..."
+
+cd obs_decam
+git pull 
+setup -t $tag -r .
+scons install declare --tag=current > /dev/null
+
+cd ..
+
+echo "Updating daf_ingest..."
+
+cd daf_ingest
+git pull
+setup -t $tag -r .
+scons install declare --tag=current > /dev/null
+
+cd ..
+
+echo "Telling the stack where to find 2MASS..."
+
+setup --nolocks -v -r ~/2mass astrometry_net_data
+
+echo "Done."
+echo "Test the installation with a command line like this one..."
+
+echo "processCcdDecam.py bulge/input/ --output bulge/test --id visit=0205343 ccdnum=1 --clobber-config -C my_config.py"
+
